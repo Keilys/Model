a23d3a15ad735e8d54552eddc57d5dde111b3728
spacy <spacy@yandex-team.ru>
2013-10-07 04:08:51 +0400

    Использую JSPromise через адаптер

---
+++ util/promise.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var JSPromise;
+
+JSPromise = require('../lib/JSPromise');
+
+exports.allResolved = JSPromise.allResolved;
+exports.create = JSPromise.create;
+exports.when = JSPromise.when;
